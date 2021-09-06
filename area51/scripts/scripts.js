9cc6d18508042df4acebd209709044f49394afc3
MaxiJRom <90151292+MaxiJRom@users.noreply.github.com>
2021-09-05 21:36:53 -0300

    Delete area51 directory

--- area51/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-let menu= document.querySelector('#nav__menu');
-
-let menu_bar=document.querySelector('#nav__bar');
-
-    menu_bar.addEventListener('click', function(){
-
-    menu.classList.toggle('menu__toggle');
- })
