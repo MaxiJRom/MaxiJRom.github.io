b8ba661942cddb11aba539614f44409eda74b1b1
MaxiJRom <90151292+MaxiJRom@users.noreply.github.com>
2021-09-07 18:55:43 -0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+let menu= document.querySelector('#nav__menu');
+
+let menu_bar=document.querySelector('#nav__bar');
+
+    menu_bar.addEventListener('click', function(){
+
+    menu.classList.toggle('menu__toggle');
+ })
